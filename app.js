dd0cb46ce9d9e1b235ee7abef87ff751f72acf8c
Yu Yongwoo <uyu423@gmail.com>
2016-09-03 22:40:21 +0900

    0.1.0 update

--- app.js
+++
@@ -1,8 +0,0 @@
-import Minesweeper from './classes/Minesweeper';
-
-const Row = 30;
-const Col = 30;
-const MineCnt = 100;
-
-//Minesweeper(Row, Col, MineCnt);
-const app = new Minesweeper(Row, Col, MineCnt);
